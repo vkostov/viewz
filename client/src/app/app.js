0a0eb06711527e7875bddde11ffa47ee38ab3738
Vasil Kostov <vasil.kostov@gmail.com>
2014-01-16 09:34:49 -0600

    Creating project file structure

--- client/src/app/app.js
+++ client/src/app/app.js
@@ -1,2 +1,34 @@
 angular.module('app', [
-    'ngRoute']);
+    'ngRoute',
+    'templates.app',
+    'templates.common'
+]);
+
+angular.module('app').controller('AppCtrl', ['$scope', function($scope) {
+
+}]);
+
+
+angular.module('app').controller('HeaderCtrl', ['$scope', '$location', '$route',
+  function ($scope, $location, $route) {
+  $scope.location = $location;
+
+  $scope.home = function () {
+     $location.path('/dashboard');
+  };
+
+  $scope.isNavbarActive = function (navBarPath) {
+       return false;
+  };
+
+  $scope.hasPendingRequests = function () {
+     return false;
+  };
+
+}]);
+
+angular.module('app').controller('ToolbarCtrl', ['$scope', '$location', '$route',
+   function ($scope, $location, $route) {
+
+   }
+]);
